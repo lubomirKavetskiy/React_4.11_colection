9ed3787bcbfd15495c75108757d62edc5a24edcd
lubomirKavetskiy <lubomirkavetskiy@gmail.com>
2017-07-04 00:03:03 +0300

    finish

--- src/new.js
+++ src/new.js
@@ -5,14 +5,13 @@ export default class NewComponent extends Component {
         super(props);
     }
 
-    renderItem(obj){
-        console.log("obj:", obj);
-        // console.log("index:", index);
+    renderItem(obj, index){
+        // 2) //console.log("obj:", obj);
+        // 2) //console.log("index:", index);
         return(
             <li key={index}><b>{obj.text}</b> - {obj.id}</li>
         );
     }
-
 
     render() {
         // 1) //console.log("this:", this.props.arr);
